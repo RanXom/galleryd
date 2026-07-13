561ab3a87a7d6880c994abfdfcbc94780d54f843
RanXom <saiyedshizain@gmail.com>
2026-07-13 13:00:40 +0530

    feat(thumbnail): initialize thumbnail package

---
+++ internal/thumbnail/generator.go
@@ -0,0 +1,26 @@
+package thumbnail
+
+import (
+	"context"
+
+	"github.com/RanXom/galleryd/internal/gallery"
+)
+
+// Generator creates thumbnails for gallery photos
+type Generator struct {
+	cacheDir string
+}
+
+func New(cacheDir string) *Generator {
+	return &Generator{
+		cacheDir: cacheDir,
+	}
+}
+
+// Generate creates (or retrieves) a thumbnail
+func (g *Generator) Generate(
+	ctx context.Context,
+	photo gallery.Photo,
+) (Thumbnail, error) {
+	return Thumbnail{}, nil
+}
