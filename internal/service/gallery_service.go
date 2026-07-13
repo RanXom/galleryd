a06e00d332aff7ddc667331c8f83b7317f5ca933
RanXom <saiyedshizain@gmail.com>
2026-07-13 18:16:22 +0530

    feat(service): build gallery index

--- internal/service/gallery_service.go
+++ internal/service/gallery_service.go
@@ -13,5 +13,21 @@ func (s *galleryService) Gallery(ctx context.Context) ([]gallery.Photo, error) {
 		return nil, err
 	}
 
-	return s.builder.Build(ctx, files)
+	photos, err := s.builder.Build(ctx, files)
+	if err != nil {
+		return nil, err
+	}
+
+	index := galleryIndex{
+		photos: photos,
+		byID:   make(map[string]gallery.Photo, len(photos)),
+	}
+
+	for _, photo := range photos {
+		index.byID[photo.ID] = photo
+	}
+
+	s.index = index
+
+	return s.index.photos, nil
 }
